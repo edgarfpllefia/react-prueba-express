1937fd5714836f8ef2fa3bc78e51cd23ab185642
edgarfpllefia <morenocomasedgar@fpllefia.com>
2026-04-14 18:06:35 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  const [equipos, setEquipos] = useState([]);
+
+  async function leerEquipos() {
+    const ruta = "http://localhost:3000/api/equipos";
+    const equiposJson = await fetch(ruta);
+    const equipos = await equiposJson.json();
+    setEquipos(equipos.equipos);
+  }
+
+  useEffect(() => {
+    console.log("useEffect");
+    leerEquipos();
+  }, []);
+
+  return (
+    <div>
+      <h1>Equipos</h1>
+      <p>{JSON.stringify(equipos)}</p>
+    </div>
+  );
+}
+
+export default App;
